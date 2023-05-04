20bd9bfe4ebd6c9117917931fce754efcc14b157
TristanGermain <2157984@cegepmontpetit.ca>
2023-05-04 09:43:48 -0400

    TODO #02 Ajout de l’interface IChanson

---
+++ R25TP05/BaladeurMultiFormats/IChanson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaladeurMultiFormats
+{
+    public interface IChanson
+    {
+        int Annee { get; }
+        string Artiste { get; }
+        string Format { get; }
+        string NomFichier { get; }
+        string Paroles { get; }
+        string Titre { get; }
+
+
+        void Ecrire(string pParoles);
+        void EcrireEntete(StreamWriter pobjFichier);
+        void EcrireParoles(StreamWriter pobjFichier);
+        void LireEntete();
+        void LireParoles(StreamReader pobjFichier);
+        void SauterEntete(StreamReader pobjFichier);
+
+    }
+}
